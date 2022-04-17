8873a3f135c7ce3b95ae91e7f84481882a0b3c08
Geunho Jang <39117502+Rootjang92@users.noreply.github.com>
2022-04-17 13:33:23 +0900

    Tailwind css 설정

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    // tailwind가 어디서 사용될 지 설정
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
