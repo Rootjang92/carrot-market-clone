b8bc4009094862dd382ddf09bcce34ffbcf3eefa
Geunho Jang <39117502+Rootjang92@users.noreply.github.com>
2022-04-26 18:14:35 +0900

    tailwind modifier 기초

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,9 @@ const Home: NextPage = () => {
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <div className="mx-auto mt-5 w-2/4 rounded-xl bg-blue-500 p-3 text-center text-white">
+        <button className="mx-auto mt-5 flex w-2/4 items-center justify-center rounded-xl bg-blue-500 p-3 text-white hover:bg-teal-500 hover:text-black focus:text-red-500 active:bg-yellow-500">
           Checkout
-        </div>
+        </button>
       </div>
       <div className="overflow-hidden rounded-3xl bg-white shadow-xl">
         <div className="bg-blue-500 p-6 pb-14">
@@ -58,9 +58,9 @@ const Home: NextPage = () => {
             <span className="text-xs text-gray-500">Chair</span>
             <div className="mt-3 mb-5 flex items-center justify-between">
               <div>
-                <input type="radio" />
-                <input type="radio" />
-                <input type="radio" />
+                <button className="h-5 w-5 rounded-full bg-yellow-500" />
+                <button className="h-5 w-5 rounded-full bg-indigo-500" />
+                <button className="h-5 w-5 rounded-full bg-teal-500" />
               </div>
               <div className="flex items-center space-x-5">
                 <button className="flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200  text-xl text-gray-500">
